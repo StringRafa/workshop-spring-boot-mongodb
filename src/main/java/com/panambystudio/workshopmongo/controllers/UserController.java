e86aa16b9f326500dc6be57520dba2210b05069b
Rafael <rafa.jo.souza@hotmail.com>
2022-04-30 17:28:36 -0300

    Obtendo um usuario por id e tratamento de erros

--- src/main/java/com/panambystudio/workshopmongo/controllers/UserController.java
+++ src/main/java/com/panambystudio/workshopmongo/controllers/UserController.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.panambystudio.workshopmongo.dto.UserDTO;
@@ -34,5 +36,11 @@ public class UserController {
 		User obj = userService.fromDTO(objDto);
 		obj = userService.insert(obj);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
