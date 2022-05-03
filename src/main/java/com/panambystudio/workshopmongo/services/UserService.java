9cebc641e0a4b0573bed933172b8344360d78a26
Rafael <rafa.jo.souza@hotmail.com>
2022-05-03 14:28:46 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/panambystudio/workshopmongo/services/UserService.java
+++ src/main/java/com/panambystudio/workshopmongo/services/UserService.java
@@ -30,6 +30,12 @@ public class UserService {
 		return obj;
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+	}
+	
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
@@ -43,5 +49,10 @@ public class UserService {
 	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
