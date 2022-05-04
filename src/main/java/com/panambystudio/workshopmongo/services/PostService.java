42580cd460979fef75fb7db1e4ff64521c96c030
Rafael <rafa.jo.souza@hotmail.com>
2022-05-04 15:02:02 -0300

    Obtendo um post por id

---
+++ src/main/java/com/panambystudio/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.panambystudio.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.panambystudio.workshopmongo.entities.Post;
+import com.panambystudio.workshopmongo.entities.User;
+import com.panambystudio.workshopmongo.repositories.PostRepository;
+import com.panambystudio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> user = postRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+	}
+
+}
