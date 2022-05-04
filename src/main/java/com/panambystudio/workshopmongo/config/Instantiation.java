ea6a62b26b185fd6e7592f6f8067a202f571d815
Rafael <rafa.jo.souza@hotmail.com>
2022-05-04 14:16:59 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/panambystudio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/panambystudio/workshopmongo/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.panambystudio.workshopmongo.dto.AuthorDTO;
 import com.panambystudio.workshopmongo.entities.Post;
 import com.panambystudio.workshopmongo.entities.User;
 import com.panambystudio.workshopmongo.repositories.PostRepository;
@@ -35,11 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User pedro = new User(null, "Pedro Souza", "pedros@gmail.com");
 		User antonio = new User(null, "Antônio Silva", "antonios@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("04/05/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("03/05/2022"), "De rolê", "De rolê na quebrada", pedro);
+		userRepository.saveAll(Arrays.asList(maria, pedro, antonio));
+		
+		Post post1 = new Post(null, sdf.parse("04/05/2022"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("03/05/2022"), "De rolê", "De rolê na quebrada", new AuthorDTO(pedro));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		userRepository.saveAll(Arrays.asList(maria, pedro, antonio));
 	}
 
 }
